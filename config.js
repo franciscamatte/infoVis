b99e264a65d171e5b7569cf94911c8289bd1514d
Arturo Pacheco <arturoph3@gmail.com>
2026-06-16 10:52:31 -0400

    PRUEBAAA

---
+++ config.js
@@ -0,0 +1,15 @@
+Protobject.setProduction(true);
+
+Protobject.initialize([
+  {
+    name: "Pantalla",
+    page: "index.html",
+    main: true,
+    debug: "master",
+  },
+  {
+    name: "Sensor",
+    page: "sensor.html",
+    debug: "local",
+  }
+]);
